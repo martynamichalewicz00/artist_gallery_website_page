31dea3dc94fc01f43188bcf40fb1c5301c0a6de9
martynamichalewicz00 <mmichalewicz95@gmail.com>
2023-01-04 12:21:28 +0100

    new

--- index.php
+++ index.php
@@ -25,9 +25,7 @@
         <br>Art & Design
       </h1>
       <p class="sub">passion to provide high quality<br>pieces of art</p>
-
-        <a href="#menu" class="fill button">see my work</a>
-
+        <button onclick="topFunction()" class="fill button">see my work</button>
     </div>
     <div class="column">
       <img 
@@ -36,7 +34,7 @@
   </div>
 </section>
 
-<section id="menu" class="gallery">
+<section class="gallery">
   <div>
     <div>
       <h1 class="my-work">My work</h1>
@@ -53,7 +51,7 @@
               ?>   <!---- its a loop [change the folder name on img path]----->                
                          <div class="lg:w-1/3 sm:w-1/2 p-4">
                         <div class="flex relative">
-                          <img alt="gallery" src="gallery/<?php echo $file; ?>">
+                          <img onclick='show()' alt="gallery" src="gallery/<?php echo $file; ?>">
                         </div>
                       </div>
              <?php
@@ -76,29 +74,24 @@
       <a>
         <span>Jan Nowak</span>
       </a>
-      <p>Znajdź mnie na:</p>
+      <p>Find me on:</p>
     </div>
   </div>
   <div>
     <div>
       <span>
-        <a>
+        <a href="https://www.facebook.com/">
           <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
           </svg>
         </a>
-        <a class="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
-        <a class="ml-3 text-gray-500">
+        <a href="https://www.instagram.com/">
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
           </svg>
         </a>
-        <a class="ml-3 text-gray-500">
+        <a href="https://www.linkedin.com/">
           <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
             <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
             <circle cx="4" cy="4" r="2" stroke="none"></circle>
---
+++ js/main.js
@@ -0,0 +1,4 @@
+function topFunction() {
+  document.body.scrollTop = 800;
+  document.documentElement.scrollTop = 800; 
+}
